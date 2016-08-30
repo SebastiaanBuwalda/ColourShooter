c27b2755bb62bbcd36ed883ef715e56ffa1f4f41
ezowolf <wiggler@live.nl>
2016-08-30 16:03:23 +0200

    Optimization, Shooting AI

---
+++ Assets/Scripts/DestroyAfterTime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfterTime : MonoBehaviour 
+{
+	[SerializeField]
+	private float destroyTime = 10;
+
+	void Start()
+	{
+		StartCoroutine (DestroyTimer ());
+	}
+
+	IEnumerator DestroyTimer()
+	{
+		yield return new WaitForSeconds (destroyTime);
+		Destroy (this.gameObject);
+	}
+}
---
+++ Assets/Scripts/SpawnObjectsInIntervals.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObjectsInIntervals : MonoBehaviour 
+{
+	[SerializeField]
+	private float startUpTime = 5;
+
+	[SerializeField]
+	private float interval = 1;
+
+	[SerializeField]
+	private GameObject[] possibleObjects;
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine (StartUp ());
+	}
+
+
+	IEnumerator StartUp()
+	{
+		yield return new WaitForSeconds (startUpTime);
+		StartCoroutine (SpawnObject ());
+	}
+
+	IEnumerator SpawnObject()
+	{
+		Instantiate (possibleObjects [Random.Range (0, possibleObjects.Length)],transform.position,Quaternion.identity);
+		yield return new WaitForSeconds (interval);
+		StartCoroutine (SpawnObject ());
+	}
+}
